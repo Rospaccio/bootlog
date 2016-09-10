63c14566dd67d41b4ef7e6ee9485d397d7e27f00
Rospaccio <alberto.mercati@gmail.com>
2016-09-10 09:45:48 +0200

    Initial commit

---
+++ src/main/java/xyz/codevomit/BootlogApplication.java
@@ -0,0 +1,12 @@
+package xyz.codevomit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootlogApplication.class, args);
+	}
+}
