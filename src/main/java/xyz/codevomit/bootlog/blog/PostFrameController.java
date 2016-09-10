5e569c0640b1222631d48e55754f3a1fd783b0da
Rospaccio <alberto.mercati@gmail.com>
2016-09-10 16:03:15 +0200

    Add initial JPA entities and repos

--- src/main/java/xyz/codevomit/bootlog/blog/PostFrameController.java
+++ src/main/java/xyz/codevomit/bootlog/blog/PostFrameController.java
@@ -2,6 +2,8 @@ package xyz.codevomit.bootlog.blog;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,8 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PostFrameController
 {
     @RequestMapping(path = {"/", ""})
-    public String postFrame()
+    public String postFrame(Model model)
     {
+        log.info("Base URL requested, no specific post");
+        model.addAttribute("prefix", "");
+        return "post-frame";
+    }
+    
+    @RequestMapping("/{postId}")
+    public String showPost(@PathVariable(value = "postId")String postId,
+        Model model)
+    {
+        log.info("Request post with id = " + postId);
+        model.addAttribute("postId", postId);
+        model.addAttribute("prefix", "../");
         return "post-frame";
     }
 }
